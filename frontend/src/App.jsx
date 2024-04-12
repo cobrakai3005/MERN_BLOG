4e1d473d7210fddcbd2af84cac83d0d5a34b497d
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2024-04-12 17:02:35 +0530

    Install Reactjs and tailwindcss and create first template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <div className="">
+      <h2 className='text-3xl'>HEllo</h2>
+    </div>
+  )
+}
+
+export default App
