ecd5a475cc1ba0527d8f5f46365f0feb01c718ce
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2024-04-12 20:58:08 +0530

    Create  pages and routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
+import { Outlet, createRoutesFromElements } from 'react-router-dom'
+import { Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Layout from './pages/Layout';
 
 
 
 function App() {
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home/>} />
+        <Route path='about' element={<About/>} />
+        <Route path='signin' element={<Signin />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='dashboard' element={<Dashboard />} />
+      </Route>
+    )
+  )
 
   return (
-    <div className="">
-      <h2 className='text-3xl'>HEllo</h2>
-    </div>
+   <>
+   <RouterProvider router={router}/>
+   </>
   )
 }
 
