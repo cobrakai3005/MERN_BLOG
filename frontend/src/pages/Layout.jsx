f154d43ec940e72033e465599331aabffc341ba3
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2024-04-12 22:37:00 +0530

    Create a header section

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 export default function Layout() {
   return (
     <div>
-        <Outlet/>
+      <Header/>
+      <Outlet/>
     </div>
   )
 }
